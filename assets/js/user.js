8623ad23c7d27563db0e748092dd8c449b8bcafe
root404-ir <mohammadrg360@gmail.com>
2024-10-15 07:16:32 +0330

    add click event with adduser

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
+
+    const $ = document
+    const addUserBtn = $.querySelector('.add-user-btn')
+    const addUserModal = $.querySelector('.add-user-modal')
+    const addUserModalBack = $.querySelector('.add-user-modal-back')
+    addUserBtn.addEventListener('click', () => {
+        addUserModal.classList.add('show')
+        addUserModalBack.classList.remove('d-none')
+    })
+    addUserModalBack.addEventListener('click', (e) => {
+        addUserModal.classList.remove('show')
+        e.target.classList.add('d-none')
+    })
 })
